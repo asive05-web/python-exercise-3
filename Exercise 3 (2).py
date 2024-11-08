3e12000d8b419f97fc78f51450815cfd69eaeae3
asive05-web <danielasivesimoki05@gmail.com>
2024-11-08 11:11:01 +0200

    python exercise 3

---
+++ Exercise 3 (2).py
@@ -0,0 +1,58 @@
+# Question 1: Basic Function Definition and Calling
+
+# TODO: Define a function called 'greet' that prints "Hello, World!"
+def greet (greet):
+    print("Hello"+greet+"how is life? ")
+
+# TODO: Call the 'greet' function
+
+greet(" world ")
+#------------------------------------------------------------------------------------
+# Question 2: Function with Parameters
+
+# TODO: Define a function called 'personalized_greeting' that takes a name as a parameter and prints a personalized greeting
+def personalized_greeting (name):
+    print("Hi"+name+"how are you doing buddy? ")
+
+
+# TODO: Call the 'personalized_greeting' function with your name
+personalized_greeting(" Asive ")
+
+#------------------------------------------------------------------------------------
+# Question 3: Function with Return Value
+
+# TODO: Define a function called 'square' that takes a number as a parameter and returns its square
+def square (number): 
+    return square 
+
+
+# TODO: Call the 'square' function with the number 5 and print the result
+square = 5 
+results= square * square 
+print(results)
+
+#------------------------------------------------------------------------------------
+# Question 4: Function with Multiple Parameters and Return Value
+# TODO: Define a function called 'rectangle_area' that takes length and width as parameters and returns the area of the rectangle
+def rectangle_area(length, width):
+    return rectangle_area
+# TODO: Call the 'rectangle_area' function with length 4 and width 5, and print the result
+width = 5
+length = 4
+results2 = length * width
+print(results2)
+#------------------------------------------------------------------------------------
+# Question 5: Using a Function as an Argument
+
+# TODO: Define a function called 'apply_operation' that takes a function and a number as parameters, and returns the result of applying the function to the number
+def apply_operation(function, number):
+    return apply_operation
+
+
+
+# TODO: Define a function called 'double' that takes a number as a parameter and returns its double
+def double(number):
+    return(number*2)
+# TODO: Use the 'apply_operation' function with the 'double' function and the number 7, and print the result
+
+# TODO: Use the 'apply_operation' function with the 'square' function (defined in Question 3) and the number 3, and print the result
